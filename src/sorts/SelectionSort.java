5bdf08ab18f845c283518a7ffd32b1a198ad6c6e
ytaoCrow <kafka880407@gmail.com>
2022-08-10 11:45:10 +0800

    註解選擇排序法

--- src/sorts/SelectionSort.java
+++ src/sorts/SelectionSort.java
@@ -5,13 +5,23 @@ import java.util.Arrays;
 public class SelectionSort {
 
     public static void selectionSort(int[] array){
+        //執行陣列長度的次數
         for(int i = 0; i < array.length;i++){
+
+            //宣告minIndex指向i，也就是陣列索引i = 0，第0個位置開始
             int minIndex = i;
+
+            //宣告陣列索引為j，並放入i的第二個位置，執行陣列長度的次數loop
             for(int j = i+1; j < array.length;j++){
+
+                //判斷i第二個位置的數值小於i的起始位置
                 if (array[j] < array[minIndex]) {
+
+                    //則不交換
                     minIndex = j;
                 }
             }
+            //如果上面判斷失敗，判斷 i 不等於 j(minIndex)，則開始交換位置
             if (i != minIndex){
                 int temp = array[i];
                 array[i] = array[minIndex];
